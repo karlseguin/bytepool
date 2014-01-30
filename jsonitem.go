29c88285e770d57615110face50f9870b8cf420a
Karl Seguin <karl@openmymind.io>
2014-01-30 17:45:19 +0700

    format

--- jsonitem.go
+++ jsonitem.go
@@ -1,127 +1,141 @@
 package bytepool
 
 import (
-  "strconv"
-  "strings"
+	"strconv"
+	"strings"
 )
 
 type JsonItem struct {
-  *Item
-  depth int
-  added bool
-  pool *JsonPool
+	*Item
+	depth int
+	added bool
+	pool  *JsonPool
 }
 
 func newJsonItem(capacity int, pool *JsonPool) *JsonItem {
-  return &JsonItem {
-    pool: pool,
-    Item: newItem(capacity, nil),
-  }
+	return &JsonItem{
+		pool: pool,
+		Item: newItem(capacity, nil),
+	}
 }
 
-var JsonEncode = func (s string) string {
-  return strings.Replace(s, `"`, `\"`, -1)
+var JsonEncode = func(s string) string {
+	return strings.Replace(s, `"`, `\"`, -1)
 }
 
 func (item *JsonItem) WriteString(s string) int {
-  return item.WriteSafeString(JsonEncode(s))
+	return item.WriteSafeString(JsonEncode(s))
 }
 
 func (item *JsonItem) WriteInt(value int) int {
-  n := item.Item.WriteString(strconv.Itoa(value))
-  return item.delimit(n)
+	n := item.Item.WriteString(strconv.Itoa(value))
+	return item.delimit(n)
 }
 
 func (item *JsonItem) WriteBool(value bool) int {
-  n := item.Item.WriteString(strconv.FormatBool(value))
-  return item.delimit(n)
+	n := item.Item.WriteString(strconv.FormatBool(value))
+	return item.delimit(n)
 }
 
 func (item *JsonItem) WriteSafeString(s string) int {
-  return item.writeString(s, true)
+	return item.writeString(s, true)
 }
 
 func (item *JsonItem) Write(b []byte) int {
-  n, _ := item.Item.Write(b)
-  return item.delimit(n)
+	n, _ := item.Item.Write(b)
+	return item.delimit(n)
 }
 
 func (item *JsonItem) WriteKeyString(key, value string) int {
-  return item.WriteKeySafeString(key, JsonEncode(value))
+	return item.WriteKeySafeString(key, JsonEncode(value))
 }
 
 func (item *JsonItem) WriteKeySafeString(key, value string) int {
-  return item.writeKeyValue(key, `"` + value + `"`)
+	return item.writeKeyValue(key, `"`+value+`"`)
 }
 
 func (item *JsonItem) WriteKeyInt(key string, value int) int {
-  return item.writeKeyValue(key, strconv.Itoa(value))
+	return item.writeKeyValue(key, strconv.Itoa(value))
 }
 
 func (item *JsonItem) WriteKeyBool(key string, value bool) int {
-  return item.writeKeyValue(key, strconv.FormatBool(value))
+	return item.writeKeyValue(key, strconv.FormatBool(value))
 }
 
 func (item *JsonItem) WriteKeyArray(key string) int {
-  n := item.writeString(key, false)
-  if item.WriteByte(byte(':')) { n++ }
-  if item.BeginArray() { n++ }
-  return n
+	n := item.writeString(key, false)
+	if item.WriteByte(byte(':')) {
+		n++
+	}
+	if item.BeginArray() {
+		n++
+	}
+	return n
 }
 
 func (item *JsonItem) WriteKeyObject(key string) int {
-  n := item.writeString(key, false)
-  if item.WriteByte(byte(':')) { n++ }
-  if item.BeginObject() { n++ }
-  return n
+	n := item.writeString(key, false)
+	if item.WriteByte(byte(':')) {
+		n++
+	}
+	if item.BeginObject() {
+		n++
+	}
+	return n
 }
 
 func (item *JsonItem) writeKeyValue(key, value string) int {
-  n := item.writeString(key, false)
-  if item.WriteByte(byte(':')) { n++ }
-  n += item.Item.WriteString(value)
-  return item.delimit(n)
+	n := item.writeString(key, false)
+	if item.WriteByte(byte(':')) {
+		n++
+	}
+	n += item.Item.WriteString(value)
+	return item.delimit(n)
 }
 
 func (item *JsonItem) writeString(s string, delimit bool) int {
-  n := item.Item.WriteString(`"` + s + `"`)
-  if delimit == false { return n }
-  return item.delimit(n)
+	n := item.Item.WriteString(`"` + s + `"`)
+	if delimit == false {
+		return n
+	}
+	return item.delimit(n)
 }
 
 func (item *JsonItem) BeginArray() bool {
-  item.added = false
-  item.depth++
-  return item.WriteByte('[')
+	item.added = false
+	item.depth++
+	return item.WriteByte('[')
 }
 
 func (item *JsonItem) EndArray() bool {
-  item.depth--
-  item.TrimLastIf(',')
-  return item.WriteByte(']')
+	item.depth--
+	item.TrimLastIf(',')
+	return item.WriteByte(']')
 }
 
 func (item *JsonItem) BeginObject() bool {
-  item.depth++
-  return item.WriteByte('{')
+	item.depth++
+	return item.WriteByte('{')
 }
 
 func (item *JsonItem) EndObject() bool {
-  item.depth--
-  item.TrimLastIf(',')
-  return item.WriteByte('}')
+	item.depth--
+	item.TrimLastIf(',')
+	return item.WriteByte('}')
 }
 
 func (item *JsonItem) delimit(length int) int {
-  if item.depth == 0 { return length }
-  item.WriteByte(byte(','))
-  return length + 1
+	if item.depth == 0 {
+		return length
+	}
+	item.WriteByte(byte(','))
+	return length + 1
 }
 
 func (item *JsonItem) Close() error {
-  item.Item.Close()
-  if item.pool != nil {
-    item.pool.list <- item
-  }
-  return nil
+	item.Item.Close()
+	if item.pool != nil {
+		item.pool.list <- item
+	}
+	return nil
 }
--- jsonitem_test.go
+++ jsonitem_test.go
@@ -1,136 +1,136 @@
 package bytepool
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestJsonCanWriteAnEncodedString(t *testing.T) {
-  expected := `"over \"9000\""`
-  item := newJsonItem(100, nil)
-  item.WriteString(`over "9000"`)
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `"over \"9000\""`
+	item := newJsonItem(100, nil)
+	item.WriteString(`over "9000"`)
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonCanWriteAString(t *testing.T) {
-  expected := `"over "9000""`
-  item := newJsonItem(100, nil)
-  item.WriteSafeString(`over "9000"`)
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `"over "9000""`
+	item := newJsonItem(100, nil)
+	item.WriteSafeString(`over "9000"`)
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonWritesAnEmptyArray(t *testing.T) {
-  expected := "[]"
-  item := newJsonItem(100, nil)
-  item.BeginArray()
-  item.EndArray()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := "[]"
+	item := newJsonItem(100, nil)
+	item.BeginArray()
+	item.EndArray()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonWritesASingleValueArray(t *testing.T) {
-  expected := "[90]"
-  item := newJsonItem(100, nil)
-  item.BeginArray()
-  item.WriteInt(90)
-  item.EndArray()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := "[90]"
+	item := newJsonItem(100, nil)
+	item.BeginArray()
+	item.WriteInt(90)
+	item.EndArray()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonWritesAMultiValueArray(t *testing.T) {
-  expected := `[90,false,"abc",true]`
-  item := newJsonItem(100, nil)
-  item.BeginArray()
-  item.WriteInt(90)
-  item.WriteBool(false)
-  item.WriteString("abc")
-  item.WriteBool(true)
-  item.EndArray()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `[90,false,"abc",true]`
+	item := newJsonItem(100, nil)
+	item.BeginArray()
+	item.WriteInt(90)
+	item.WriteBool(false)
+	item.WriteString("abc")
+	item.WriteBool(true)
+	item.EndArray()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonWritesAnEmptyObject(t *testing.T) {
-  expected := `{}`
-  item := newJsonItem(100, nil)
-  item.BeginObject()
-  item.EndObject()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `{}`
+	item := newJsonItem(100, nil)
+	item.BeginObject()
+	item.EndObject()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonWritesADelimitedByte(t *testing.T) {
-  expected := `["abc","123"]`
-  item := newJsonItem(100, nil)
-  item.BeginArray()
-  item.Write([]byte(`"abc"`))
-  item.Write([]byte(`"123"`))
-  item.EndArray()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `["abc","123"]`
+	item := newJsonItem(100, nil)
+	item.BeginArray()
+	item.Write([]byte(`"abc"`))
+	item.Write([]byte(`"123"`))
+	item.EndArray()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonASingleValueObject(t *testing.T) {
-  expected := `{"over":"90\"00!"}`
-  item := newJsonItem(100, nil)
-  item.BeginObject()
-  item.WriteKeyString("over", "90\"00!")
-  item.EndObject()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `{"over":"90\"00!"}`
+	item := newJsonItem(100, nil)
+	item.BeginObject()
+	item.WriteKeyString("over", "90\"00!")
+	item.EndObject()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonAMultiValueObject(t *testing.T) {
-  expected := `{"name":"goku","power":9000,"over":true}`
-  item := newJsonItem(100, nil)
-  item.BeginObject()
-  item.WriteKeySafeString("name", "goku")
-  item.WriteKeyInt("power", 9000)
-  item.WriteKeyBool("over", true)
-  item.EndObject()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `{"name":"goku","power":9000,"over":true}`
+	item := newJsonItem(100, nil)
+	item.BeginObject()
+	item.WriteKeySafeString("name", "goku")
+	item.WriteKeyInt("power", 9000)
+	item.WriteKeyBool("over", true)
+	item.EndObject()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
 
 func TestJsonNestedObjects(t *testing.T) {
-  expected := `[1,{"name":"goku","levels":[2,{"over":{"9000":"!"}}]}]`
-  item := newJsonItem(100, nil)
-  item.BeginArray()
-  item.WriteInt(1)
-  item.BeginObject()
-  item.WriteKeyString("name","goku")
-  item.WriteKeyArray("levels")
-  item.WriteInt(2)
-  item.BeginObject()
-  item.WriteKeyObject("over")
-  item.WriteKeyString("9000", "!")
-  item.EndObject()
-  item.EndObject()
-  item.EndArray()
-  item.EndObject()
-  item.EndArray()
-  actual := item.String()
-  if actual != expected {
-    t.Errorf("Expecting %q, got %q", expected, actual)
-  }
+	expected := `[1,{"name":"goku","levels":[2,{"over":{"9000":"!"}}]}]`
+	item := newJsonItem(100, nil)
+	item.BeginArray()
+	item.WriteInt(1)
+	item.BeginObject()
+	item.WriteKeyString("name", "goku")
+	item.WriteKeyArray("levels")
+	item.WriteInt(2)
+	item.BeginObject()
+	item.WriteKeyObject("over")
+	item.WriteKeyString("9000", "!")
+	item.EndObject()
+	item.EndObject()
+	item.EndArray()
+	item.EndObject()
+	item.EndArray()
+	actual := item.String()
+	if actual != expected {
+		t.Errorf("Expecting %q, got %q", expected, actual)
+	}
 }
